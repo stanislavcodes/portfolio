4a17a9f54f675a30aa71e6faa9f7e987d7a6dafe
stanislavcodes <korchevsky2004@gmail.com>
2020-06-28 19:15:12 +0300

    gulp build update

--- gulpfile.js
+++ gulpfile.js
@@ -72,3 +72,4 @@ function watchTask() {
 // Runs the scss and js tasks simultaneously
 // then runs cacheBust, then watch task
 exports.default = series(parallel(scssTask, jsTask), cacheBustTask, watchTask);
+exports.build = series(parallel(scssTask, jsTask), cacheBustTask);
