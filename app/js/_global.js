d27c762b440e097e7341553b95e96a10320047f8
stanislavcodes <korchevsky2004@gmail.com>
2020-06-30 07:04:52 +0300

    js nav fix

--- app/js/_global.js
+++ app/js/_global.js
@@ -1,27 +1,16 @@
 // Global
-app.global = {
-  init: function () {
-    // Load all global functions here
-    console.log("load global functions");
-    app.global.loadHeader();
-  },
-  loadHeader: function () {
-    // Some specific function
-    console.log("loadHeader()");
-  },
-};
+// app.global = {
+//   init: function () {
+//     // Load all global functions here
+//     console.log("load global functions");
+//     app.global.loadHeader();
+//   },
+//   loadHeader: function () {
+//     // Some specific function
+//     console.log("loadHeader()");
+//   },
+// };
 
-let menu = document.querySelector(".menu");
-let nav = document.querySelector(".nav");
-let logo = document.querySelector(".logo__link");
-
-menu.addEventListener("click", function (e) {
-  e.preventDefault();
-  document.body.classList.toggle('locked');
-  logo.classList.toggle("open");
-  menu.classList.toggle("open");
-  nav.classList.toggle("open");
-});
 
 // Run the global stuff
-app.global.init();
+// app.global.init();
--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,16 @@
 console.log("Load script.js");
 
+let menu = document.querySelector(".menu");
+let nav = document.querySelector(".nav");
+let logo = document.querySelector(".logo__link");
+
+menu.addEventListener("click", function (e) {
+  e.preventDefault();
+  document.body.classList.toggle("locked");
+  logo.classList.toggle("open");
+  menu.classList.toggle("open");
+  nav.classList.toggle("open");
+});
+
 // Instantiating the global app object
-var app = {};
+// var app = {};
